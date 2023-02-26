468a319f87429e3bd777b46a417f6707e771c361
suee97 <atn1su@gmail.com>
2023-02-26 20:25:19 +0900

    feat: add custom button class

---
+++ seoultechSC/Helpers/Extensions.swift
@@ -0,0 +1,12 @@
+import Foundation
+import UIKit
+
+// Custom Colors
+extension UIColor {
+    static var primaryPurple: UIColor {
+        return UIColor(red: 124/255, green: 135/255, blue: 242/255, alpha: 1)
+    }
+    static var secondaryPurple: UIColor {
+        return UIColor(red: 190/255, green: 197/255, blue: 255/255, alpha: 1)
+    }
+}
---
+++ seoultechSC/Views/ActionButton.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+class ActionButton: UIButton {
+    
+    // width, height 기본값 : 320, 50
+    // 버튼 색상 기본값 : primaryPurple
+    init(title: String, width: CGFloat = 320, height: CGFloat = 50, backgroundColor: UIColor = .primaryPurple) {
+        super.init(frame: .zero)
+        setTitle(title, for: .normal)
+        self.widthAnchor.constraint(equalToConstant: width).isActive = true
+        self.heightAnchor.constraint(equalToConstant: height).isActive = true
+        self.layer.cornerRadius = 5
+        self.backgroundColor = backgroundColor
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
