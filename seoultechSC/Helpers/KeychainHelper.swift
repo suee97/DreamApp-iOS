467504ee47ce1e2c9c29aa867e597937ccd77c8e
suee97 <atn1su@gmail.com>
2023-03-26 00:53:31 +0900

    [#14] feat: item중복시 덮어쓰도록 함수 수정

--- seoultechSC/Helpers/KeychainHelper.swift
+++ seoultechSC/Helpers/KeychainHelper.swift
@@ -8,34 +8,87 @@ class KeychainHelper {
     
     func saveAccessToken(_ token: String) {
         let data = token.data(using: .utf8)
-        
         let query: [String: Any] = [
             kSecClass as String: kSecClassGenericPassword,
             kSecAttrService as String: "ACCESS_TOKEN",
-            kSecValueData as String: data
         ]
-        
-        let status = SecItemAdd(query as CFDictionary, nil)
-        guard status == errSecSuccess else {
-            print("Failed to save token to keychain: \(status)")
+
+        var item: CFTypeRef?
+        let status = SecItemCopyMatching(query as CFDictionary, &item)
+
+        switch status {
+        case errSecSuccess:
+            // Item exists in keychain, update it
+            let updateQuery: [String: Any] = [
+                kSecValueData as String: data
+            ]
+            let updateStatus = SecItemUpdate(query as CFDictionary, updateQuery as CFDictionary)
+            guard updateStatus == errSecSuccess else {
+                print("Failed to update token in keychain: \(updateStatus)")
+                return
+            }
+            print("Refresh Token updated successfully")
+        case errSecItemNotFound:
+            // Item doesn't exist in keychain, add it
+            let addQuery: [String: Any] = [
+                kSecValueData as String: data,
+                kSecAttrAccessible as String: kSecAttrAccessibleAfterFirstUnlock
+            ]
+            let addStatus = SecItemAdd(addQuery as CFDictionary, nil)
+            guard addStatus == errSecSuccess else {
+                print("Failed to save token to keychain: \(addStatus)")
+                return
+            }
+            print("Access Token saved successfully: ")
+        default:
+            print("Error retrieving token from keychain: \(status)")
             return
         }
+        
+        print("new access token : \(token)")
+
     }
     
     func saveRefreshToken(_ token: String) {
         let data = token.data(using: .utf8)
-        
         let query: [String: Any] = [
             kSecClass as String: kSecClassGenericPassword,
             kSecAttrService as String: "REFRESH_TOKEN",
-            kSecValueData as String: data
         ]
-        
-        let status = SecItemAdd(query as CFDictionary, nil)
-        guard status == errSecSuccess else {
-            print("Failed to save token to keychain: \(status)")
+
+        var item: CFTypeRef?
+        let status = SecItemCopyMatching(query as CFDictionary, &item)
+
+        switch status {
+        case errSecSuccess:
+            // Item exists in keychain, update it
+            let updateQuery: [String: Any] = [
+                kSecValueData as String: data
+            ]
+            let updateStatus = SecItemUpdate(query as CFDictionary, updateQuery as CFDictionary)
+            guard updateStatus == errSecSuccess else {
+                print("Failed to update token in keychain: \(updateStatus)")
+                return
+            }
+            print("Refresh Token updated successfully: ")
+        case errSecItemNotFound:
+            // Item doesn't exist in keychain, add it
+            let addQuery: [String: Any] = [
+                kSecValueData as String: data,
+                kSecAttrAccessible as String: kSecAttrAccessibleAfterFirstUnlock
+            ]
+            let addStatus = SecItemAdd(addQuery as CFDictionary, nil)
+            guard addStatus == errSecSuccess else {
+                print("Failed to save token to keychain: \(addStatus)")
+                return
+            }
+            print("Refresh Token saved successfully")
+        default:
+            print("Error retrieving token from keychain: \(status)")
             return
         }
+
+        print("new refresh token : \(token)")
     }
     
     func getAccessToken() -> String? {
@@ -84,42 +137,8 @@ class KeychainHelper {
         return String(data: data, encoding: .utf8)
     }
     
-    func resetAccessToken() {
-        let token: String = ""
-        let data = token.data(using: .utf8)
-        
-        let query: [String: Any] = [
-            kSecClass as String: kSecClassGenericPassword,
-            kSecAttrService as String: "ACCESS_TOKEN",
-            kSecValueData as String: data
-        ]
-        
-        let status = SecItemAdd(query as CFDictionary, nil)
-        guard status == errSecSuccess else {
-            print("Failed to save token to keychain: \(status)")
-            return
-        }
-    }
-    
-    func resetRefreshToken() {
-        let token: String = ""
-        let data = token.data(using: .utf8)
-        
-        let query: [String: Any] = [
-            kSecClass as String: kSecClassGenericPassword,
-            kSecAttrService as String: "REFRESH_TOKEN",
-            kSecValueData as String: data
-        ]
-        
-        let status = SecItemAdd(query as CFDictionary, nil)
-        guard status == errSecSuccess else {
-            print("Failed to save token to keychain: \(status)")
-            return
-        }
-    }
-    
     func resetAccessRefreshToken() {
-        self.resetAccessToken()
-        self.resetRefreshToken()
+        self.saveAccessToken("")
+        self.saveRefreshToken("")
     }
 }
