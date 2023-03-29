164e872c8923b411704d43eecf5a74706764b191
suee97 <atn1su@gmail.com>
2023-03-29 10:43:33 +0900

    [#23] EventCollectionView 생성 및 Cell연결

--- seoultechSC/Controllers/Home/event/EventViewController.swift
+++ seoultechSC/Controllers/Home/event/EventViewController.swift
@@ -1,9 +1,111 @@
 import UIKit
+import Alamofire
+import SwiftUI
 
-class EventViewController: UIViewController {
-
+class EventViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    // MARK: - Properties
+    let LRSpacing: CGFloat = screenWidth * (20/360)
+    let cellWidth: CGFloat = screenWidth * (155/360)
+    let cellHeight: CGFloat = screenWidth * (155/360) * (220/155)
+    
+    private lazy var collectionViewLayout: UICollectionViewFlowLayout = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.itemSize = CGSize(width: cellWidth, height: cellHeight)
+        return layout
+    }()
+    
+    private lazy var eventCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout)
+        collectionView.register(EventCollectionViewCell.self, forCellWithReuseIdentifier: "event_collectionview_cell")
+        collectionView.backgroundColor = .white
+        return collectionView
+    }()
+    
+    
+    // MARK: - Lifecycle
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureNavigationBar()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
+        configureCollectionViewDelegate()
+    }
+    
+    
+    // MARK: - Helpers
+    private func configureNavigationBar() {
+        self.navigationController?.navigationBar.isHidden = false
+        let na = customNavBarAppearance()
+        self.navigationController?.navigationBar.scrollEdgeAppearance = na
+        self.navigationController?.navigationBar.compactAppearance = na
+        self.navigationController?.navigationBar.standardAppearance = na
+        if #available(iOS 15.0, *) {
+            navigationController?.navigationBar.compactScrollEdgeAppearance = na
+        }
+        
+        self.navigationItem.title = "이벤트 참여"
+        self.navigationController?.navigationBar.tintColor = .white
+    }
+    
+    private func configureUI() {
+        view.backgroundColor = .white
+        
+        view.addSubview(eventCollectionView)
+        
+        eventCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            eventCollectionView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: LRSpacing),
+            eventCollectionView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -LRSpacing),
+            eventCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: screenHeight * (22/640)),
+            eventCollectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0)
+        ])
+    }
+    
+    private func configureCollectionViewDelegate() {
+        eventCollectionView.delegate = self
+        eventCollectionView.dataSource = self
+    }
+    
+    
+    // MARK: - Selectors
+    
+    
+    
+    // MARK: - Functions
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "event_collectionview_cell", for: indexPath) as! EventCollectionViewCell
+        cell.titleLabel.text = "title \(indexPath.row)"
+        return cell
+    }
+    
+}
+
+
+#if DEBUG
+struct ViewControllerRepresentable: UIViewControllerRepresentable {
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
 
     }
+    @available(iOS 13.0, *)
+    func makeUIViewController(context: Context) -> some UIViewController {
+        EventViewController()
+    }
 }
+
+struct ViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewControllerRepresentable()
+    }
+}
+
+#endif
---
+++ seoultechSC/Views/EventCollectionViewCell.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+class EventCollectionViewCell: UICollectionViewCell {
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .green
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+    private func configureUI() {
+        contentView.backgroundColor = .modalBackground
+        
+        contentView.addSubview(titleLabel)
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            titleLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
+    }
+}
