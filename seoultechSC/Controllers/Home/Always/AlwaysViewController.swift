aff66af84fd893eeec0b0ae31e3f75028d755b3c
tkddn0518 <tkddn0518@naver.com>
2023-04-01 14:39:45 +0900

    - 상시사업 페이지 추가

--- seoultechSC/Controllers/Home/Always/AlwaysViewController.swift
+++ seoultechSC/Controllers/Home/Always/AlwaysViewController.swift
@@ -1,13 +1,8 @@
-//
-//  AlwaysViewController.swift
-//  seoultechSC
-//
-//  Created by 변상우 on 2023/03/27.
-//
-
 import UIKit
 
-class AlwaysViewController: UIViewController {
+class AlwaysViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    private var itemList = ["캐노피", "듀라테이블","앰프&마이크","리드선","L카","의자"]
     
     private let scrollView : UIScrollView = UIScrollView()
     
@@ -54,6 +49,54 @@ class AlwaysViewController: UIViewController {
         return myReservation
     }()
     
+    private let collectionView : UICollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    
+    private let infoContainer : UIView = {
+        let view = UIView()
+        
+        let title = UILabel()
+        title.text = "안내사항"
+        title.font = UIFont(name: "Pretendard-Bold", size: 16)
+        
+        view.addSubview(title)
+        
+        title.translatesAutoresizingMaskIntoConstraints = false
+        title.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        let info1 = UILabel()
+        let info2 = UILabel()
+        
+        info1.text = "1. 대여 물품이 파손되었을 시, 수리 비용의 80%를 대여인(또는 대여 기구) 측에서 비용하고 나머지 20%는 총학생회 자치회비에서 부담한다."
+        info2.text = "2. 파손에 대해 수리가 불가하다고 판단될 시, 대여인(또는 대여 기구)에서 같은 제품 또는 그에 걸맞는 비용을 부담하여야 한다."
+        
+        let labelArr: [UILabel] = [info1, info2]
+        
+        for i in labelArr {
+            view.addSubview(i)
+            i.translatesAutoresizingMaskIntoConstraints = false
+            i.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+            
+            i.font = UIFont(name: "Pretendard-Regular", size: 12)
+            i.textColor = .text_caption
+            i.numberOfLines = 3
+            
+            NSLayoutConstraint.activate([
+                i.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                i.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+            ])
+        }
+        
+        NSLayoutConstraint.activate([
+            title.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            title.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            title.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            info1.topAnchor.constraint(equalTo: title.bottomAnchor, constant: 7),
+            info2.topAnchor.constraint(equalTo: info1.bottomAnchor, constant: 3),
+        ])
+    
+        return view
+    }()
+    
 
     // MARK: - Lifecycle
     override func viewWillAppear(_ animated: Bool) {
@@ -63,6 +106,11 @@ class AlwaysViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(AlwaysCollectionViewCell.self, forCellWithReuseIdentifier: AlwaysCollectionViewCell.identifier)
+        
         configureUI()
         
     }
@@ -88,17 +136,22 @@ class AlwaysViewController: UIViewController {
         scrollView.backgroundColor = .clear
         contentView.backgroundColor = .clear
         myInfoContainer.backgroundColor = .white
+        collectionView.backgroundColor = .clear
         
         view.addSubview(scrollView)
         scrollView.addSubview(contentView)
         
         contentView.addSubview(myInfoContainer)
         contentView.addSubview(myReservationButton)
+        contentView.addSubview(collectionView)
+        contentView.addSubview(infoContainer)
         
         scrollView.translatesAutoresizingMaskIntoConstraints = false
         contentView.translatesAutoresizingMaskIntoConstraints = false
         myInfoContainer.translatesAutoresizingMaskIntoConstraints = false
         myReservationButton.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        infoContainer.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
@@ -117,6 +170,14 @@ class AlwaysViewController: UIViewController {
             myReservationButton.topAnchor.constraint(equalTo: myInfoContainer.topAnchor, constant: 119),
             myReservationButton.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 20),
             myReservationButton.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -20),
+            collectionView.topAnchor.constraint(equalTo: myReservationButton.bottomAnchor, constant: 30),
+            collectionView.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 20),
+            collectionView.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -20),
+            collectionView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -181),
+            infoContainer.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            infoContainer.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 20),
+            infoContainer.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -20),
+            infoContainer.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -136),
         ])
         
         contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
@@ -126,10 +187,36 @@ class AlwaysViewController: UIViewController {
         contentViewHeight.isActive = true
         
     }
+    
+    // CollectionView Datasource
+    
+    // 셀 갯수
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return itemList.count
+    }
+    
+    // 셀 지정
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AlwaysCollectionViewCell.identifier, for: indexPath) as? AlwaysCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.backgroundColor = .white
+        cell.layer.cornerRadius = 10
+        return cell
+    }
+    
+    // CollectionView DelegateFlowLayout
+    
+    // 셀 크기 설정
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 74, height: 100)
+    }
+    
+    
 
     
     // MARK: - Selectors
     @objc private func myReservationBtn() {
-        print("logout")
+        print("MyReservation Confirm")
     }
 }
