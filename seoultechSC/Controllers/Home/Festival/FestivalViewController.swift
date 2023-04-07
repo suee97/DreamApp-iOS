188c897f5f19ed3e99f3b1dca64592e3d2540a56
suee97 <atn1su@gmail.com>
2023-04-08 03:29:58 +0900

    feat: google map 세팅 & 지도 연결

--- seoultechSC/Controllers/Home/Festival/FestivalViewController.swift
+++ seoultechSC/Controllers/Home/Festival/FestivalViewController.swift
@@ -1,13 +1,23 @@
 import UIKit
+import GoogleMaps
 
-class FestivalViewController: UIViewController {
-
+class FestivalViewController: UIViewController, GMSMapViewDelegate {
+    
+    // MARK: - Properties
+    private var mapView: GMSMapView!
+    
+    
+    // MARK: - Lifecycle
+    override func loadView() {
+        configureMap()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureNavigationBar()
         configureUI()
     }
-
+    
     // MARK: - Helpers
     private func configureNavigationBar() {
         self.navigationController?.navigationBar.isHidden = false
@@ -18,12 +28,21 @@ class FestivalViewController: UIViewController {
         if #available(iOS 15.0, *) {
             navigationController?.navigationBar.compactScrollEdgeAppearance = na
         }
-
+        
         self.navigationItem.title = "어의대동제"
         self.navigationController?.navigationBar.tintColor = .white
     }
-
+    
     private func configureUI() {
         view.backgroundColor = .white
+    }
+    
+    private func configureMap() {
+        // Load the map at set latitude/longitude and zoom level
+        let camera:GMSCameraPosition = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 11)
+        
+        mapView = GMSMapView(frame: .zero, camera: camera)
+        self.view = mapView
+        mapView.delegate = self
     }
 }
