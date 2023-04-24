10453671c2b1d8a36f3818bb71841e8d70baf4e3
suee97 <atn1su@gmail.com>
2023-04-24 14:35:10 +0900

    design: shadow radius수정

--- seoultechSC/Controllers/Home/HomeViewController.swift
+++ seoultechSC/Controllers/Home/HomeViewController.swift
@@ -378,7 +378,7 @@ class HomeTabButton: UIButton {
         self.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
         self.layer.shadowOpacity = 1
         self.layer.shadowOffset = CGSize.zero
-        self.layer.shadowRadius = 4
+        self.layer.shadowRadius = 3
         
         if icon != "🤝🏻" && icon != "🎟️" {
             self.layer.cornerRadius = 20
